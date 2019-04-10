5f029a11016d9a726e5c55181b865e8316fcf15c
egor5q <31898567+egor5q@users.noreply.github.com>
2019-04-10 23:24:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,11 @@ except Exception as e:
     print('Ошибка:\n', traceback.format_exc())
     bot.send_message(441399484, traceback.format_exc())
 
+    
+    
+    
+    
+    
 print('7777')
 bot.polling(none_stop=True,timeout=600)
 
